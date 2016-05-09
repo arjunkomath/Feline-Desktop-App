e471d45ee67e81654f147e7462409649d4a947f7
Arjun Komath <arjunkomath@gmail.com>
2016-05-09 15:19:34 +0530

    image slider

--- app/src/components/post-widget.js
+++ app/src/components/post-widget.js
@@ -1,6 +1,7 @@
 var React = require('react');
 import {Pane, Icon, Button} from "react-photonkit";
 var Loader = require('react-loader');
+var Slider = require('react-slick');
 
 var Comments = require('./comments');
 
@@ -13,6 +14,41 @@ var styles = {
 
 module.exports = React.createClass({
 
+    renderSlide: function () {
+        var slides = [];
+        var i=0;
+        this.props.post.media.map(function (m) {
+            i++;
+            if (m.media_type == 'image')
+                slides.push(<div key={i} style={{
+                backgroundImage: 'url("'+m.image_url+'")',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                height: 300,
+                width: '100%'
+                }}></div>)
+        })
+        return slides;
+    },
+
+    renderSlider: function () {
+        if (!this.props.load)
+            return false;
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        return (
+            <Slider {...settings}>
+                {this.renderSlide()}
+            </Slider>
+        );
+    },
+
     openUrl: function (url) {
         console.log(url);
         window.open(url);
@@ -22,17 +58,23 @@ module.exports = React.createClass({
         return (
             <Pane>
                 <Loader loaded={this.props.load}>
+
                     <div style={styles.post}>
+
+                        {this.renderSlider()}
+
                         <h3>{this.props.post.name}</h3>
                         <p>{this.props.post.tagline}</p>
 
                         <p>
                             <Icon glyph="up-open-big" title="up-open-big"/> {this.props.post.votes_count}
-                            <Icon style={{marginLeft: 10}} glyph="comment" title="comment"/> {this.props.comments.length}
-                            <Button style={{marginLeft: 10}} ptStyle="primary" text="GET IT" onClick={() => this.openUrl(this.props.post.redirect_url)} />
+                            <Icon style={{marginLeft: 10}} glyph="comment"
+                                  title="comment"/> {this.props.comments.length}
+                            <Button style={{marginLeft: 10}} ptStyle="primary" text="GET IT"
+                                    onClick={() => this.openUrl(this.props.post.redirect_url)}/>
                         </p>
 
-                        <Comments comments={this.props.comments} />
+                        <Comments comments={this.props.comments}/>
 
                     </div>
                 </Loader>
